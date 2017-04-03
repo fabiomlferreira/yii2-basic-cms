38fd84ad8c2d0db8622889efe7bc6436b984bf3b
fabiomlferreira <fabiomlferreira@hotmail.com>
2017-04-03 18:47:08 +0100

    Commit inicial

---
+++ common/config/main.php
@@ -0,0 +1,120 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        /*'assetManager' => [
+            'appendTimestamp' => true,
+            'bundles' => [
+                'yii\web\JqueryAsset' => [
+                    'js' => [
+                        'jquery.min.js'
+                        //YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+                    ],
+                    'jsOptions' => ['position' => \yii\web\View::POS_HEAD] // activate to put jquery on head
+                ],
+                'frontend\assets\ModernizrAsset'=> [
+                    'js' => [
+                        'js/modernizr/modernizr.min.js'
+                        //YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+                    ],
+                    'jsOptions' => ['position' => \yii\web\View::POS_HEAD] // activate to put jquery on head
+                ],
+                'yii\bootstrap\BootstrapPluginAsset' => [
+                    'js' => [
+                        'js/bootstrap.min.js'
+                    ]
+                ],
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [
+                        'css/bootstrap.min.css'
+                    ]
+                ],
+            ],
+        ],*/
+        
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+            'cachePath' => '@common/runtime/cache', //caminho comum para guardar a cache para conseguir apagar atravez do backend
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager',
+	    'defaultRoles' => ['admin','voter','user'], // here define your roles
+            'itemFile' => '@console/data/items.php', //Default path to items.php | NEW CONFIGURATIONS
+            'assignmentFile' => '@console/data/assignments.php', //Default path to assignments.php | NEW CONFIGURATIONS
+	    'ruleFile' => '@console/data/rules.php', //Default path to rules.php | NEW CONFIGURATIONS
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            
+        ],
+        /*'config' => [
+            'class' => 'common\components\Config'
+        ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\DbMessageSource',
+                    'db' => 'db',
+                    'sourceLanguage' => 'en-US', // Developer language
+                    'sourceMessageTable' => '{{%language_source}}',
+                    'messageTable' => '{{%language_translate}}',
+                    'cachingDuration' => 86400,
+                    'enableCaching' => false,
+                    'forceTranslation' => true,
+                ],
+                'app' => [
+                    'class' => 'yii\i18n\DbMessageSource',
+                    'db' => 'db',
+                    'sourceLanguage' => 'en-US', // Developer language
+                    'sourceMessageTable' => '{{%language_source}}',
+                    'messageTable' => '{{%language_translate}}',
+                    'forceTranslation' => true,
+                    'cachingDuration' => 86400,
+                    'enableCaching' => false,
+                ],
+                /*'user' => [
+                    'class' => 'yii\i18n\DbMessageSource',
+                    'db' => 'db',
+                    'sourceLanguage' => 'en-US', // Developer language
+                    'sourceMessageTable' => '{{%language_source}}',
+                    'messageTable' => '{{%language_translate}}',
+                    'forceTranslation' => true,
+                    'cachingDuration' => 86400,
+                    'enableCaching' => false,
+                ],*//*
+            ],
+        ],*/
+    ],
+    'modules' => [
+        'filemanager' => [
+            'class' => 'fabiomlferreira\filemanager\Module',
+            //'class' => 'pendalf89\filemanager\Module',
+            'rename' => true, //permite ter fazer upload de fotos com o mesmo nome
+            'autoUpload' => true,
+            'optimizeOriginalImage' => true,
+            'maxSideSize' => 1200,
+            'thumbnailOnTheFly' => false,  //if is true the component thumbnail should be used
+            // Upload routes
+            'routes' => [
+                // Base absolute path to web directory
+                'baseUrl' => '',
+                // Base web directory url
+                'basePath' => '@frontend/web',
+                // Path for uploaded files in web directory
+                'uploadPath' => 'uploads',
+            ],
+            // Thumbnails info
+            'thumbs' => [ 
+                'default' => [
+                    'name' => 'default',
+                    'size' => [125, 125],
+                ],
+                'small_square' => [
+                    'name' => 'medium_square',
+                    'size' => [300, 300],
+                ],
+            ],
+        ],
+    ]
+];
